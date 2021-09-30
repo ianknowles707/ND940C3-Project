6ee5c329554cbbadcb4ace96b6d9c158dffa3bfb
ianknowles707 <72895539+ianknowles707@users.noreply.github.com>
2021-09-30 17:10:39 -0400

    Complete functionality.

--- app/src/main/java/com/udacity/LoadButton.kt
+++ app/src/main/java/com/udacity/LoadButton.kt
@@ -13,10 +13,22 @@ class LoadButton @JvmOverloads constructor(
 
     //Define variables for the animation
     private var currentWidth = 0f
+    private var angle = 0f
+    private var radius = 0f
     private var maxWidth = 0f
     private var buttonHeight = 0f
+
+    private var rectX = 0f
+    private var rectY = 0f
+    private var circleLeft = 0f
+    private var circleRight = 0f
+    private var circleTop = 0f
+    private var circleBottom = 0f
+
     private var buttonText = ""
     private lateinit var animator: ValueAnimator
+    private lateinit var animatorCircle: ValueAnimator
+
     private var animationRunning = false
 
     //Define the button base - lazy ensure it will be set when called (after onSizeChanged)
@@ -29,6 +41,8 @@ class LoadButton @JvmOverloads constructor(
     //so will be passed later
     private lateinit var rectAnimated: RectF
 
+    private lateinit var circleAnimated: RectF
+
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
@@ -37,12 +51,12 @@ class LoadButton @JvmOverloads constructor(
     }
 
     //Set up actions for the custom button based on button state
-    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { _, _, new ->
         when (new) {
             ButtonState.Loading -> {
                 //Change text on button, start the animations
                 buttonText = resources.getString(R.string.download_button_loading)
-                animateLoadButton()
+                animateAll()
             }
 
             ButtonState.Clicked -> {
@@ -54,9 +68,13 @@ class LoadButton @JvmOverloads constructor(
                 buttonText = resources.getString(R.string.download_button_default)
                 if (animationRunning) {
                     animator.end()
+                    animatorCircle.end()
                 }
                 currentWidth = 0f
+                angle = 0f
+
                 rectAnimated = RectF(0f, 0f, currentWidth, buttonHeight)
+                circleAnimated = RectF(circleLeft, circleTop, circleRight, circleBottom)
             }
         }
         invalidate()
@@ -70,12 +88,17 @@ class LoadButton @JvmOverloads constructor(
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         maxWidth = w.toFloat()
         buttonHeight = h.toFloat()
+        radius = buttonHeight / 4
+        rectX = (maxWidth / 2)
+        rectY = (buttonHeight / 2)
+        circleLeft = maxWidth - 3 * radius
+        circleRight = maxWidth - radius
+        circleTop = radius
+        circleBottom = buttonHeight - radius
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val x = (maxWidth / 2)
-        val y = (buttonHeight / 2)
 
         paint.color = Color.GRAY
         canvas?.drawRect(rectBase, paint)
@@ -83,8 +106,17 @@ class LoadButton @JvmOverloads constructor(
         paint.color = Color.BLUE
         canvas?.drawRect(rectAnimated, paint)
 
+        paint.color = Color.YELLOW
+        canvas?.drawArc(circleLeft, circleTop, circleRight, circleBottom, 0f, angle, true, paint)
+
         paint.color = Color.WHITE
-        canvas?.drawText(buttonText, x, y, paint)
+        canvas?.drawText(buttonText, rectX, rectY, paint)
+    }
+
+    private fun animateAll() {
+        animateLoadButton()
+        animateCircle()
+        animationRunning = true
     }
 
     private fun animateLoadButton() {
@@ -98,11 +130,17 @@ class LoadButton @JvmOverloads constructor(
             invalidate()
         }
         animator.start()
-        animationRunning = true
     }
 
-    private fun stopAnimation() {
-        animator.end()
-        animationRunning = false
+    private fun animateCircle() {
+        animatorCircle = ValueAnimator.ofFloat(0f, 360f)
+        animatorCircle.duration = 2000
+        animatorCircle.repeatMode = ValueAnimator.RESTART
+        animatorCircle.repeatCount = ValueAnimator.INFINITE
+        animatorCircle.addUpdateListener { value ->
+            angle = value.animatedValue as Float
+            invalidate()
+        }
+        animatorCircle.start()
     }
 }
